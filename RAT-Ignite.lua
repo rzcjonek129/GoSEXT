90ca154e2e40cf79144ede6612b0b6818728c056
rzcjonek129 <jonatanu3@gmail.com>
2017-03-21 00:05:07 +0100

    Update RAT-Ignite.lua

--- RAT-Ignite.lua
+++ RAT-Ignite.lua
@@ -8,7 +8,7 @@ function AutoUpdate(data)
 end
 GetWebResultAsync("https://raw.githubusercontent.com/rzcjonek129/GoSEXT/master/Version%20Folder/RAT-Ignite.version", AutoUpdate)
 
-PrintChat('Thanks for using RATzone Ignite go to site: http://ratzone.eu')
+PrintChat("Thanks for using RATzone Ignite go to site: http://ratzone.eu")
 local ignite = nil
 
 if GetCastName(myHero, SUMMONER_1):lower():find("summonerdot") then
